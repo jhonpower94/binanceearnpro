72b154eb9ce825ff067cb086764d76f9cde65478
ghost <jhonsnow751@gmail.com>
2020-06-25 18:59:55 -0700

    added dshboard layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { useEffect } from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import { deepOrange } from "@material-ui/core/colors";
 import { Router } from "@reach/router";
 import HomeLayout from "./pages/homepage";
 import DashboardLayout from "./pages/dashboard";
 import AccountLayout from "./pages/account";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: deepOrange[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: "#ffffff",
+    },
+  },
+});
 
 function App() {
   useEffect(() => {}, []);
 
   return (
-    <Router>
-      <HomeLayout path="/" />
-      <DashboardLayout path="dashboard" />
-      <AccountLayout path="account" />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <HomeLayout path="/" />
+        <DashboardLayout path="dashboard" />
+        <AccountLayout path="account" />
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,13 +1,215 @@
-import React, { useEffect } from "react";
-import { HomePageStrings } from "../../lang/langhomepage";
+import React from "react";
+import "./dashboard.css";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { deepOrange } from "@material-ui/core/colors";
 
-function DashboardLayout() {
-  useEffect(() => {}, []);
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    fontFamily: "Roboto",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  navtext: {
+    color: deepOrange[500],
+  },
+}));
+
+function DashboardLayout(props) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [selecteditem, setSelecteditem] = React.useState(0);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const handleListItemClick = (index, link) => {
+    setSelecteditem(index);
+  };
+
+  const drawer = (
+    <div>
+      <List>
+        {[
+          {
+            name: "Features",
+            link: "features",
+            icon: (
+              <InboxIcon color={selecteditem === 0 ? "primary" : "inherit"} />
+            ),
+          },
+          {
+            name: "Affiliates",
+            link: "affiliates",
+            icon: (
+              <MailIcon color={selecteditem === 1 ? "primary" : "inherit"} />
+            ),
+          },
+          {
+            name: "Banking Info",
+            link: "info",
+            icon: (
+              <InboxIcon color={selecteditem === 2 ? "primary" : "inherit"} />
+            ),
+          },
+        ].map((links, index) => (
+          <ListItem
+            button
+            key={index}
+            onClick={() => handleListItemClick(index, links.link)}
+          >
+            <ListItemIcon>{links.icon}</ListItemIcon>
+            <ListItemText
+              primary={links.name}
+              className={selecteditem === index ? classes.navtext : null}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <React.Fragment>
-      <p>Dashboard page</p>
-    </React.Fragment>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar color="secondary" position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          {useMediaQuery(useTheme().breakpoints.up("sm")) ? (
+            <img
+              src={require("../../images/logodesktop.svg")}
+              alt="logo"
+              width="150px"
+            />
+          ) : (
+            <img
+              src={require("../../images/mobile.svg")}
+              alt="logo"
+              height="50px"
+            />
+          )}
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+    </div>
   );
 }
 
