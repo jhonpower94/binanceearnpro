af8caf0c74aacacf158dd37d8df82d8ed5ce21aa
ghost <jhonsnow751@gmail.com>
2022-12-13 01:30:29 +0100

    dsfsdfsdfdsffeKHKH

--- src/pages/homepage-binance/faq.js
+++ src/pages/homepage-binance/faq.js
@@ -4,84 +4,86 @@ import "./fonts/font.min.scoped.css";
 import "./styles.scoped.css";
 
 function Faqs() {
- 
-
-  return <>
-  <section class="pagetitle">
-      <div class="title">
-        <h2>Faq</h2>
-      </div>
-    </section>
-    
-    <section class="bodycontent">
-      <div class="datapages">
-        <div class="texttitle">What is our goal?</div>
-        <p>
-          Unchainedtrade investment proposes a profitable Cooperation for all
-          investors. Which is highly recognised in the fields of technology and
-          innovations. Our technicality are centred on developing methods and
-          effective algorithms of Bitcoin mining.
-        </p>
-      </div>
-
-      <div class="datapages">
-        <div class="texttitle">
-          What is the duration of the company’s existence?
+  return (
+    <>
+      <section class="pagetitle">
+        <div class="title">
+          <h2>Faq</h2>
         </div>
-        <p>This company’s investment exists in perpetuity.</p>
-      </div>
+      </section>
 
-      <div class="datapages">
-        <div class="texttitle">
-          Are my personal data on this website secured?
+      <section class="bodycontent">
+        <div class="datapages">
+          <div class="texttitle">What is our goal?</div>
+          <p>
+            Unchainedtrade investment proposes a profitable Cooperation for all
+            investors. Which is highly recognised in the fields of technology
+            and innovations. Our technicality are centred on developing methods
+            and effective algorithms of Bitcoin mining.
+          </p>
         </div>
-        <p>
-          Yes, we provide maximum security for you personal data on this
-          website, which is highly encrypted and safe for investors.
-        </p>
-      </div>
 
-      <div class="datapages">
-        <div class="texttitle">Who can join your investment company?</div>
-        <p>
-          To become a client you have to register account. Enter username, set
-          password, add your email and Bitcoin address to make an account.
-        </p>
-      </div>
+        <div class="datapages">
+          <div class="texttitle">
+            What is the duration of the company’s existence?
+          </div>
+          <p>This company’s investment exists in perpetuity.</p>
+        </div>
 
-      <div class="datapages">
-        <div class="texttitle">Where can I register Bitcoin address?</div>
-        <p>
-          Visit https://blockchain.info to register unique Bitcoin
-          address.https://blockchain.info
-        </p>
-      </div>
+        <div class="datapages">
+          <div class="texttitle">
+            Are my personal data on this website secured?
+          </div>
+          <p>
+            Yes, we provide maximum security for you personal data on this
+            website, which is highly encrypted and safe for investors.
+          </p>
+        </div>
 
-      <div class="datapages">
-        <div class="texttitle">Will I receive profit every calendar day?</div>
-        <p>
-          I gladly inform you that your profits are accrued to you every 24 hours, 7 days a week.
-        </p>
-      </div>
+        <div class="datapages">
+          <div class="texttitle">Who can join your investment company?</div>
+          <p>
+            To become a client you have to register account. Enter username, set
+            password, add your email and Bitcoin address to make an account.
+          </p>
+        </div>
 
-      <div class="datapages">
-        <div class="texttitle">Are you paying even on weekends?</div>
-        <p>Yes, of course, we are paying on weekends and holidays.</p>
-      </div>
+        <div class="datapages">
+          <div class="texttitle">Where can I register Bitcoin address?</div>
+          <p>
+            Visit https://blockchain.info to register unique Bitcoin
+            address.https://blockchain.info
+          </p>
+        </div>
 
-      <div class="datapages">
-        <div class="texttitle">What’s the duration of my deposit?</div>
-        <p>
-          The time span of your deposit exists in perpetuation.
-        </p>
-      </div>
+        <div class="datapages">
+          <div class="texttitle">Will I receive profit every calendar day?</div>
+          <p>
+            I gladly inform you that your profits are accrued to you every 24
+            hours, 7 days a week.
+          </p>
+        </div>
 
-      <div class="datapages">
-        <div class="texttitle">What currency can I invest?</div>
-        <p>We only accept Bitcoins, etherium and litecoin.</p>
-      </div>
-    </section>
-    </>;
+        <div class="datapages">
+          <div class="texttitle">Are you paying even on weekends?</div>
+          <p>Yes, of course, we are paying on weekends and holidays.</p>
+        </div>
+
+        <div class="datapages">
+          <div class="texttitle">What’s the duration of my deposit?</div>
+          <p>The time span of your deposit exists in perpetuation.</p>
+        </div>
+
+        <div class="datapages">
+          <div class="texttitle">What currency can I invest?</div>
+          <p>
+            We only accept Bitcoin, Ethereum, USDT, BNB, Perfect money and
+            Shiba
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Faqs;
